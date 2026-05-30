e2ffb9389a66e0f49f3f0677ef709361636105fc
Admin <Admin@LAPTOP-JO2INC4N.lan>
2026-05-30 10:08:54 +0530

    commit

---
+++ corejavaprogram/src/com/tnsif/accessmodifierprogram/Demo1.java
@@ -0,0 +1,21 @@
+package com.tnsif.accessmodifierprogram;// user defined pacakge
+
+import com.tnsif.accessmodifierprogram.Publicdemo;
+
+public class Demo1 {// class creation
+        
+        String h="roshn";
+        String H="roshn";
+        
+public static void main(String[]args) {// main method
+        
+        Publicdemo p=new Publicdemo();
+        
+        System.out.println(p.a);
+        //System.out.println(p.b);
+        System.out.println(p.s);
+        
+        
+        System.out.println("welcome to java");
+}
+}
--- corejavaprogram/src/packagesbasic/demo.java
+++ corejavaprogram/src/packagesbasic/demo.java
@@ -1,6 +1,10 @@
-package packagesbasic;
+package packagesbasic;//user defined pacakges
 
 public class demo {//class creation
+	
+	String h="kumar";
+	String H="kumar";
+	
 public static void main(String[]args) {//main method
 	System.out.println("welcome to java ");
 }
